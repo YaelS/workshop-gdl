6158fc3f4f3b00fc700f55387b4ceaa3cf19f6f1
Carlos Yael Salcedo Espinosa <carlosyael89@hotmail.com>
2013-06-12 23:50:42 -0500

    se agrego opcion de editar archivo con errores

--- workshop/credit-bureau/src/c/cb.c
+++ workshop/credit-bureau/src/c/cb.c
@@ -11,95 +11,152 @@
 #include <winsock2.h>
 #include <unistd.h>
 
-#define PORT 3550 /* El puerto que será abierto */
-#define BACKLOG 2 /* El número de conexiones permitidas */
-#define HEADERS_LENGTH 78 /* Numero de caracteres de la cabecera del archivo Loans.txt */
-#define RFC_LENGTH 10 /* Numero de caracteres del RFC */
-#define BUFFER_LENGTH 11 /* Tamaño del buffer para recibir mensaje */
-#define LINE_LENGTH 100 /* Tamaño del arreglo para guardar linea por linea del archivo Loans.txt*/
-#define THE_FILE "Loans.txt" /* Archivo para abrir */
+#define PORT 3550 				/* El puerto que será abierto */
+#define BACKLOG 2 				/* El número de conexiones permitidas */
+#define HEADERS_LENGTH 78 		/* Numero de caracteres de la cabecera del archivo Loans.txt */
+#define RFC_LENGTH 10 			/* Numero de caracteres del RFC */
+#define BUFFER_LENGTH 11 		/* Tamaño del buffer para recibir mensaje */
+#define LINE_LENGTH 100 		/* Tamaño del arreglo para guardar linea por linea del archivo Loans.txt*/
+#define THE_FILE "Loans.txt" 	/* Archivo para abrir */
+
+char* searchRFC(char line[LINE_LENGTH], char buffer[BUFFER_LENGTH]){
+
+	int searchCount;
+	int searchBegin;
+	
+	/* Se recorre el contador hasta la posicion inicial del RFC */
+	for(searchBegin = 0; line[searchBegin] != '|'; searchBegin++){}
+	
+	searchBegin++; 	/* Se aumenta uno el contador para que apunte al primer caracter del RFC */
+	printf("\nComparando primer caracter del RFC: %c", line[searchBegin]);
+	printf("\nPrimer caracter del buffer: %c", buffer[0]);
+	/* Se compara caracter por caracter el buffer con los rfc de cada registro */
+	for(searchCount = 0; searchCount < RFC_LENGTH && buffer[searchCount] == line[searchCount+searchBegin]; searchCount++){
+		
+		printf("\nComparando caracter  #%i: %c == %c",searchCount+1, line[searchCount+searchBegin], buffer[searchCount]);
+		if(searchCount == 9){ 	/* Validacion de salida, cuando todos los caracteres coincidieron */
+			
+			/* Se envia la linea encontrada */	
+			printf(" Encontrado...");							
+			return line;							
+		}
+		
+	}
+
+	return NULL;
+	
+}
 
 void doprocessing (int sock)
 {
     char buffer[BUFFER_LENGTH];
 
     int recvMsgSize = 0;
-	char opc[1]; /* Opcion que indica al programa las instrucciones de leer o escribir en Loans.txt */
+	char opc[1]; 	/* Opcion que indica al programa las instrucciones de leer o escribir en Loans.txt */
 	
 	/* Se recibe la opcion para leer o escribir en Loans */
 	while(recvMsgSize <= 0){
 		if ((recvMsgSize = recv(sock, opc, 1, 0)) < 0)
 			perror("ERROR reading to socket");
 	}
+	
+	printf("\nOpcion: %c", opc[0]);
     
     /* Receive message from client */
     if ((recvMsgSize = recv(sock, buffer, BUFFER_LENGTH, 0)) < 0)
         perror("ERROR reading to socket");
+	buffer[10] = '\0';
 		
-	printf("%s",buffer);
+	printf("\nRFC: %s", buffer);
 	
 	FILE *fp;
+	char line[LINE_LENGTH];
+	char *linep;
+	int letter;			
+	int i = 0;
 	
 	switch(opc[0]){
+	
 		case 'R':
+		
+			printf("\nOpcion leer elegida...");
 			////////////////////////////////////////////////////////READ FILE
-			fp = fopen(THE_FILE, "r"); /* Abrir archivo */
-			fseek(fp, HEADERS_LENGTH, 0); /* Recorrer cursor al principio del primer registro */
 			
-			int letter;
-			char line[LINE_LENGTH];
-			
-			int i = 0;
-			int searchCount;
-			int searchBegin;
-			boolean foundRFC = 0;
+			fp = fopen(THE_FILE, "r"); 				/* Abrir archivo */
+			fseek(fp, HEADERS_LENGTH, SEEK_SET); 	/* Recorrer cursor al principio del primer registro */			
 
+			i = 0;
+			
 			do{ 
 				letter = getc(fp); /* Obtener letra por letra */
-				if(letter != '\n'){ /* Valida que no sea fin de linea */			
-					line[i] = letter; /* Inserta letra por letra en el arreglo line */
+				
+				if(letter != '\n'){ 	/* Valida que no sea fin de linea */			
+					line[i] = letter; 	/* Inserta letra por letra en el arreglo line */
 					i++;			
-				}			
-				else{	
-					/* Se recorre el contador hasta la posicion inicial del RFC */
-					for(searchBegin = 0; line[searchBegin] != '|'; searchBegin++){}
-					searchBegin++; /* Se aumenta uno el contador para que apunte al primer caracter del RFC */
-					printf("\n%c",line[searchBegin]);
-					/* Se compara caracter por caracter el buffer con los rfc de cada registro */
-					for(searchCount = 0; searchCount < RFC_LENGTH && buffer[searchCount] == line[searchCount+searchBegin]; searchCount++){
-						printf("\n*%c == %c*",line[searchCount+searchBegin],buffer[searchCount]);
-						if(searchCount == 9){ /* Validacion de salida, cuando todos los caracteres coincidieron */
-							line[i] = '%'; /* Separador de registros encontrados */
-							/* Se envia la linea encontrada */
-							if (send(sock, line, i+1, 0) != i)
-								perror("ERROR writing to socket");
-							printf(" Encontrado...");
-						}
-					}
-					i = 0;
 				}
+				
+				else{	
+					
+					if((linep = searchRFC(line, buffer)) != NULL){	/* Se llama la funcion para buscar en el archivo */
+						line[i] = '%'; 		/* Separador de registros encontrados */
+						if (send(sock, linep, i+1, 0) != i+1)
+							perror("ERROR writing to socket");
+					}
+					i = 0;					
+				}
+				
 			}while(letter != EOF); /* Leer hasta fin de archivo */
 			
 			/* Envio del mensaje de salida */
 			if (send(sock, "\n", 1, 0) != 1)
 				perror("ERROR writing to socket");
-			/////////////////////////////////////////////////////////////READ FILE}
+				
 			break;
+			/////////////////////////////////////////////////////////////READ FILE END
+			
+			
+		case 'E':
+		
+			printf("\nOpcion editar elegida...");
+			/////////////////////////////////////////////////////////////EDIT FILE
+		
+			fp = fopen(THE_FILE, "r+");
+			
+			fseek(fp, HEADERS_LENGTH, SEEK_SET); /* Recorrer cursor al principio del primer registro */
+			
+			//fgets(line, LINE_LENGTH, fp);
+			
+			i = 0;
+			
+			do{ 
+				letter = getc(fp); /* Obtener letra por letra */
+				
+				if(letter != '\n'){ 	/* Valida que no sea fin de linea */			
+					line[i] = letter; 	/* Inserta letra por letra en el arreglo line */
+					i++;			
+				}
+				else{
+					
+					if((linep = searchRFC(line, buffer)) != NULL){
+						line[i] = '\0';
+						printf("\nLinea a editar: %s", line);
+						
+						fseek(fp, -3, SEEK_CUR);
+						
+						printf("\nCaracter a reemplazar: %c", getc(fp));
+						//fputc('N', fp);
+					}					
+					i = 0;
+				}
+			}while(letter != EOF);
+
+			break;
+			/////////////////////////////////////////////////////////////EDIT FILE END
 			
 	}
-
-    /* Send received string and receive again until end of transmission */
     
-        /* Echo message back to client */
-        /*if (send(sock, buffer, recvMsgSize, 0) != recvMsgSize)
-            perror("ERROR writing to socket");*/
-
-        /* See if there is more data to receive */
-        /*if ((recvMsgSize = recv(sock, buffer, 10, 0)) < 0)
-            perror("ERROR reading to socket");*/
-    
-
-    //closesocket(sock);    /* Close client socket */
+	fclose(fp); 			/* Cerrar archivo */
+    closesocket(sock);    	/* Close client socket */
 }
 
 BOOL initW32() 
@@ -185,12 +242,8 @@ int main()
 
       printf("Se obtuvo una conexión desde %s\n", inet_ntoa(client.sin_addr) );
       /* que mostrará la IP del cliente */
-
-      //send(fd2,"Bienvenido a mi servidor.\n",27,0);
-      /* que enviará el mensaje de bienvenida al cliente */
       
       doprocessing(fd2);
 
    } /* end while */
 }
-
